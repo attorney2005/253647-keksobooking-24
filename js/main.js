7098dd859fec0b0ad1bf5a61e9d03419dc487242
attorney2005 <attorney2005@yandex.ru>
2021-09-26 19:22:27 +0300

    Исаправил замечания

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function getRandomIntFromRange(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-alert(getRandomIntFromRange(1, 100));
+getRandomIntFromRange(1, 100);
 
 
 function getFloatFromRange(min, max, range) {
@@ -18,7 +18,7 @@ function getFloatFromRange(min, max, range) {
   const randomNumber = (Math.random() * (max - min + 1) + min);
   return randomNumber.toFixed(range);
 }
-alert(getFloatFromRange(0, 100, 3));
+getFloatFromRange(0, 100, 3);
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://schoolsw3.com/jsref/jsref_tofixed.php
