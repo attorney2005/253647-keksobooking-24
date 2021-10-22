0f0604438b6acc38d7339e02b268ade28ecb9fde
Вадим Байбуз <vadimbajbuz@192.168.1.6>
2021-10-22 12:55:38 +0300

    Сделал цену за ночь

--- js/form.js
+++ js/form.js
@@ -2,12 +2,8 @@ const form = document.querySelector('.ad-form');
 const titleInput = document.querySelector('#title');
 const priceInput = document.querySelector('#price');
 const typeSelect = document.querySelector('#type');
-const typeList = typeSelect.querySelectorAll('option');
-console.log(typeList);
 
 const button = document.querySelector('.ad-form__submit');
-
-
 
 form.setAttribute('action', 'https://24.javascript.pages.academy/keksobooking');
 form.setAttribute('method', 'POST');
@@ -21,16 +17,22 @@ priceInput.setAttribute('required', true);
 priceInput.setAttribute('number', true);
 priceInput.setAttribute('max', '10000');
 
+function setMinPriceForType() {
+    const typeToMinPrice = {
+        'bungalow': 0,
+        'flat': 1000,
+        'hotel': 3000,
+        'house': 5000,
+        'palace': 10000,
+    };
+    const type = typeSelect.value;
+    const price = typeToMinPrice[type];
+    priceInput.setAttribute('min', price);
+    priceInput.setAttribute('placeholder', price);
+}
 
-
-
-
-
-
-
-
-
-
+typeSelect.addEventListener('change', setMinPriceForType);
+setMinPriceForType();
 //тестовые данные
 // titleInput.value = 'Уютный домик в деревне Старки Московской области рядом с Черноголовкой'
 // priceInput.value = 4000;
