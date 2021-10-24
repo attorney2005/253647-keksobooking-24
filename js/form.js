8f85853a5922c34a96f3ea3144beb4dc8be8bb15
Вадим Байбуз <vadimbajbuz@192.168.1.3>
2021-10-24 18:38:01 +0300

    Сделал синхронизацию комнат с гостями

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@ const form = document.querySelector('.ad-form');
 const titleInput = document.querySelector('#title');
 const priceInput = document.querySelector('#price');
 const typeSelect = document.querySelector('#type');
+const roomsSelect = document.querySelector('#room_number');
+const guestsSelect = document.querySelector('#capacity');
 
 const button = document.querySelector('.ad-form__submit');
 
@@ -33,6 +35,61 @@ function setMinPriceForType() {
 
 typeSelect.addEventListener('change', setMinPriceForType);
 setMinPriceForType();
+
+const disableRoomSelects = function () {
+    for (var i = 0; i < guestsSelect.options.length; i++) {
+        guestsSelect.options[i].disabled = true;
+    }
+};
+
+const roomNumberChangeHandler = function () {
+    disableRoomSelects();
+    const choosenValue = (roomsSelect.value === '100') ? '0' : roomsSelect.value;
+    if (choosenValue === '0' || choosenValue === '1') {
+        for (let i = 0; i < guestsSelect.options.length; i++) {
+            if (guestsSelect.options[i].value === choosenValue) {
+                guestsSelect.options[i].disabled = false;
+            }
+        }
+    }
+    if (choosenValue === '2') {
+        for (let i = 0; i < guestsSelect.options.length; i++) {
+            if (guestsSelect.options[i].value === choosenValue || guestsSelect.options[i].value ==='1') {
+                guestsSelect.options[i].disabled = false;
+            }
+        }
+    }
+    if (choosenValue === '3') {
+        for (let i = 0; i < guestsSelect.options.length; i++) {
+            if (guestsSelect.options[i].value === choosenValue || guestsSelect.options[i].value ==='2'|| guestsSelect.options[i].value ==='1') {
+                guestsSelect.options[i].disabled = false;
+            }
+        }
+    }
+    for (var i = 0; i < guestsSelect.options.length; i++) {
+        if (!guestsSelect.options[i].disabled) {
+            guestsSelect.options[i].selected = true;
+            break;
+        }
+    }
+}
+
+roomsSelect.addEventListener('change', roomNumberChangeHandler);
+
+roomNumberChangeHandler();
+
+
+
+//     const roomToGuestMap = {
+//         '1': ['1'],
+//         '2': ['2', '1'],
+//         '3': ['3', '2', '1'],
+//         '100': ['0']
+//     };
+
+// roomsSelect.addEventListener('change', setGuestLimit);
+// setGuestLimit();
+
 //тестовые данные
 // titleInput.value = 'Уютный домик в деревне Старки Московской области рядом с Черноголовкой'
 // priceInput.value = 4000;
