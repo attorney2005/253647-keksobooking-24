07f0b39008f807b7675fea33631a84f9dd2baae5
Вадим Байбуз <vadimbajbuz@192.168.1.3>
2021-11-01 16:00:29 +0300

    Сделал гланую метку

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 import { createListOfAnnouncements } from './data.js';
 import { renderAnnouncementCard } from './template.js';
 import { initForm, activateForm } from './form.js';
-import { initMap } from './map.js';
+import { initMap} from './map.js';
 
 const announcementList = createListOfAnnouncements();
 
 initForm();
 initMap(onMapLoaded);
+
 
 function onMapLoaded() {
     activateForm();
--- js/map.js
+++ js/map.js
@@ -12,7 +12,31 @@ function initMap(onLoad) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
   ).addTo(map);
+  
+  const mainPinIcon = L.icon({
+    iconUrl: 'img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  });
+  
+  const mainPinMarker = L.marker(
+    {
+      lat: 35.65862,
+      lng: 139.74539,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+    
+  mainPinMarker.addTo(map);
+  
+  mainPinMarker.on('moveend', (evt) => {
+    console.log(evt.target.getLatLng());
+  });
 }
+
 
 export {
   initMap
